7f2a5362f06c911a9104b3506fae7b17694ee9b1
Do-code-ing <zxcvbn658@naver.com>
2021-09-25 21:07:21 +0900

    2021-09-25

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)  # 질문
+    pub_date = models.DateTimeField("date published")  # 게시 날짜
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)  # 선택 텍스트
+    votes = models.IntegerField(default=0)  # 투표 집계
